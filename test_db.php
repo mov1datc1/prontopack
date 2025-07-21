73867d4b030b4c26084016ef6dab98844cd54404
Jonathan Palacios <jonathanpalacios@192.168.1.11>
2025-07-21 12:23:43 -0600

    Actualización test db

--- test_db.php
+++ test_db.php
@@ -1,9 +1,10 @@
 <?php
 require_once 'config.php';
 
-if ($conn->connect_error) {
-    die("❌ Conexión fallida: " . $conn->connect_error);
-} else {
+try {
+    $stmt = $pdo->query("SELECT 1");
     echo "✅ Conexión exitosa a la base de datos.";
+} catch (PDOException $e) {
+    echo "❌ Error de conexión: " . $e->getMessage();
 }
 ?>
